f35fd8c551f589b147370084b7d13290f531f484
khenmarkbetito <60655020+khenmarkbetito@users.noreply.github.com>
2020-02-04 20:52:04 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,63 @@
+
+<html>
+
+<head>
+     <link href="css.css" rel="stylesheet" type="text/css">
+     <title>
+    XBYTES
+     </title>
+</head>
+
+<body>
+     <div id="header">
+     <img id="img" src="image1.jpg"/>
+         <h1>KhenMark Betito</h1>
+         <h2>TVL 4-1 </h2>
+    </div>
+    <div id="Content">
+      <div id="nav"> <!--div=Division--> 
+        <ul id="navul">
+        <ul>  <!--unordered list-->
+        <li>
+               <a id = "INDEX" href="index.php">Login</a>  
+                     
+              </li>
+                       
+               <li>  
+               <a id = "INDEX" href="#">Register</a>  
+                     
+              </li>
+                 <li>
+                     <a id = "INDEX" href="about.php">About Us</a>
+                 </li>
+              <li>
+                     <a id = "INDEX" href="contact.php">Contact us</a>
+        
+            </ul>
+
+      </div>
+<html>
+
+<head>
+  <link rel="stylesheet" href="style.css">
+  <link href="https://fonts.googleapis.com/css?family=Ubuntu" rel="stylesheet">
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
+  <link rel="stylesheet" href="path/to/font-awesome/css/font-awesome.min.css">
+  <title>Sign in</title>
+</head>
+
+<body>
+  <div class="main">
+    <p class="sign" align="center">Sign in</p>
+    <form class="form1">
+      <input class="un " type="text" align="center" placeholder="Username">
+      <input class="pass" type="password" align="center" placeholder="Password">
+      <a class="submit" align="center" href="home.php">Sign in</a>
+      <p class="forgot" align="center"><a href="#">Forgot Password?</p>
+            
+                
+    </div>
+     
+</body>
+
+</html
